631af92cce536351b1ad3759c31123cda7fae718
klg-2016 <kathrynl.grabowski@gmail.com>
2025-01-21 01:12:27 +0000

    edit this file to fix the file naming issue

--- ct_classifier/dataset.py
+++ ct_classifier/dataset.py
@@ -92,4 +92,4 @@ class CTDataset(Dataset):
         # transform: see lines 31ff above where we define our transformations
         img_tensor = self.transform(img)
 
-        return img_tensor, label
+        return img_tensor, label, image_name
